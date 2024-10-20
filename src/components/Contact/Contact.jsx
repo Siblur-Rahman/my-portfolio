7997b2bee4b654cdc46bbb4e8c8a9b130ae036d3
MS Rahman <msrahman048gmail.com>
2024-10-20 21:57:21 +0600

    footer section added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,38 @@
 import './Contact.css'
 import Heading from '../Heading/Heading'
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm('service_73cr4ni', 'template_wcx9qp9', form.current, {
+          publicKey: 'v4N3bjVhLDJkW2SBz',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
     return (
         <div className='contactSection bg-black'>
             <Heading Heading={'Contact Me'}/>
             <div className="contactDescription">
                 <h3>Please fill out form below to descuss any opportunities</h3>
             </div>
-            <form action="" className='form'>
-                <input placeholder="Your Name" className='inputName input' rows="5" cols="50"/>
+            <form action="" className='form' ref={form} onSubmit={sendEmail}>
+                <input placeholder="Your Name" name="user_name" className='inputName input' rows="5" cols="50"/>
                 <br />
-                <input placeholder="Your Email" className='input' type='email'/>
-                <textarea placeholder='Your Message' className='textarea'/>
+                <input placeholder="Your Email" name="user_email" className='input' type='email'/>
+                <textarea placeholder='Your Message' name="message" className='textarea'/>
                 <div className='submit'>
                     <button type='submit' className='submitBtn'>Submit</button>
                 </div>
